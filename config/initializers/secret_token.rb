e3f9d3102f27c8b5d7d591708457f059d2a8bc3e
elf1218 <kevinmu01@gmail.com>
2014-02-17 19:16:05 -0500

    start hw4

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Hw4::Application.config.secret_key_base = '02add3b9ad515a983529ce93f80137730ae00d4dda308a8de8e1825cab91b552317b7a48185a79c0beb1ee7e306f19ceeba63dc64c88de51c3d4318b540daeb5'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Hw4::Application.config.session_store :cookie_store, key: '_hw4_session'
